d6f2347d9f0e0ae8c3df1538735e2a73d442af68
YuShimojo <yongxiatiao@gmail.com>
2022-07-09 06:02:01 +0000

    [finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   end
 
   def update
+     @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user), notice: "You have updated user successfully."
     else
