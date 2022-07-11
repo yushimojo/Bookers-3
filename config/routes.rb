a4a52b1eb36e270645b54bba8b736ab40f745fc4
YuShimojo <yongxiatiao@gmail.com>
2022-07-11 14:01:07 +0000

    [add]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
   get "home/about"=>"homes#about"
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do 
+  resource :favorites, only: [:create, :destroy] 
+end  
   resources :users, only: [:index,:show,:edit,:update]
-  resource :favorites, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
